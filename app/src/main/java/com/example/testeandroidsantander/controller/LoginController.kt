d27cdbcee2f35270caa535ede4b9fd59f6a067a2
welli <wellington.terroni@outlook.com>
2019-04-21 22:45:36 -0300

    first commit

---
+++ app/src/main/java/com/example/testeandroidsantander/controller/LoginController.kt
@@ -0,0 +1,68 @@
+package com.example.testeandroidsantander.controller
+
+import java.util.*
+
+class LoginController {
+
+    fun validEmail(email: String): Boolean =
+        android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    fun isCPF(CPF: String): Boolean {
+        if (CPF == "00000000000" ||
+            CPF == "11111111111" ||
+            CPF == "22222222222" || CPF == "33333333333" ||
+            CPF == "44444444444" || CPF == "55555555555" ||
+            CPF == "66666666666" || CPF == "77777777777" ||
+            CPF == "88888888888" || CPF == "99999999999" ||
+            CPF.length != 11
+        )
+            return false
+
+        val dig10: Char
+        val dig11: Char
+        var sm: Int
+        var i: Int
+        var r: Int
+        var num: Int
+        var peso: Int
+
+        try {
+
+            sm = 0
+            peso = 10
+            i = 0
+            while (i < 9) {
+                num = CPF[i].toInt() - 48
+                sm += num * peso
+                peso -= 1
+                i++
+            }
+
+            r = 11 - sm % 11
+            dig10 = if (r == 10 || r == 11)
+                '0'
+            else
+                (r + 48).toChar()
+            sm = 0
+            peso = 11
+            i = 0
+
+            while (i < 10) {
+                num = CPF[i].toInt() - 48
+                sm += num * peso
+                peso -= 1
+                i++
+            }
+
+            r = 11 - sm % 11
+            dig11 = if (r == 10 || r == 11)
+                '0'
+            else
+                (r + 48).toChar()
+
+            return dig10 == CPF[9] && dig11 == CPF[10]
+        } catch (error: InputMismatchException) {
+            return false
+        }
+    }
+}
